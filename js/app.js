008c6b4b718c6b426ec67a64303c6c522a4046ad
centem <centem@gmail.com>
2020-08-01 13:46:51 +0200

    media queries

--- js/app.js
+++ js/app.js
@@ -5,64 +5,73 @@ document.documentElement.style.scrollBehavior = "smooth"
 // create ul element
 const ul = document.querySelector('#navbar__list');
 
-// get number of section elements
-const sectionCount = document.querySelectorAll('section').length;
-console.log(sectionCount);
-
-// for each section that exists create a li and a element and append them to the nav ul
-for (let i = 1; i < sectionCount+1; i++) {
-    const li = document.createElement('li');
-    const a = document.createElement('a');
-    // add href and textContent to anchor element
-    a.href = `#section${i}`;
-    a.textContent = `Section ${i}`;
-
-    // add navbar_link class that I will use in the for loop below
-    a.classList.add('navbar_link');
-    li.appendChild(a);
-    ul.appendChild(li);
-}
-
-// select all the li elements that have the .navbar_link
-const links = document.querySelectorAll('.navbar_link');
+// get number of section elements to determine how many navbar links needed
 const sections = document.querySelectorAll('section');
 
+// for each section that exists create a li and a element and append them to the nav ul
+sections.forEach(function(e) {
+    // create li and a elements to append to ul
+    const li = document.createElement('li');
+    const a = document.createElement('a');
 
-for (let i = 0; i < links.length; i++) {
+    // add href and textContent to anchor element
+    a.href = `#${e.id}`;
+
+    // use a regex to get the number part of section id to add to text content
+    a.textContent = 'Section ' + e.id.match(/(\d+)/)[0];
+
+    // add navbar_link class that I will use in the for loop below
+    li.appendChild(a);
+    ul.appendChild(li);
+});
+
+// select all the anchor tags in the ul
+const links = document.querySelectorAll('#navbar__list li a');
+
+// code for adding and removing class for menu items and sections
+for (let i = 0; i < sections.length; i++ ) {
+
     links[i].addEventListener('click', function() {
-        // first remove active_link class from all a elements
-        for (let j = 0; j < links.length; j++) {
-            links[j].classList.remove('active_link');
+        // first remove active_link and active-section class from all a and section elements
+        for (let j = 0; j < sections.length; j++) {
+            links[j].classList.remove('active-link');
             sections[j].classList.remove('active-section');
         }
-        // now add to this a element
-        links[i].classList.add('active_link');
-        document.querySelector(`#section${i+1}`).classList.add('active-section');
-    })
+        // add active-link and active-section classes
+        links[i].classList.add('active-link');
+        sections[i].classList.add('active-section');
+    });
+};
 
-    sections[i].addEventListener('scroll', function() {
-        if (section[i].getBoundingClientRect() <= 150 && section[i] >= 150) {
-            section[i].classList.add('active-section');
-        } else {
-            section[i].classList.remove('active-section');
+// helper function for scroll window event listener
+var isInViewport = function (elem) {
+	var distance = elem.getBoundingClientRect();
+        return (
+            distance.top >= 0 &&
+            distance.left >= 0 &&
+            distance.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            distance.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    };
+
+// add event listener to window object for setting active classes
+window.addEventListener('scroll', function (event) {
+    for (let i = 0; i < sections.length; i++ ) {
+        var section = document.querySelector(`#section${i+1} .landing__container`);
+        let distance = section.getBoundingClientRect();
+        if (isInViewport(section)) {
+            //console.log(section.textContent);
+            for (let j = 0; j < sections.length; j++) {
+                links[j].classList.remove('active-link');
+                sections[j].classList.remove('active-section');
+            }
+            // add active-link and active-section classes
+            links[i].classList.add('active-link');
+            sections[i].classList.add('active-section');
         }
-    })
-}
-
-function makeActive() {
-    for (const section of sections) {
-      const box = section.getBoundingClientRect();
-      console.log(box);
-      // You can play with the values in the "if" condition to further make it more accurate. 
-      if (box.top <= 150 && box.bottom >= 150) {
-        // Apply active state on the current section and the corresponding Nav link.
-      } else {
-        // Remove active state from other section and corresponding Nav link.
-      }
     }
-  }
+}, false);
 
-  makeActive();
 
 
 
